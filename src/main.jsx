d34a3af8c763e535b3ff053b6b4fd6c00ad569a2
Nutricalvo <nutricomgui@gmail.com>
2025-07-24 00:55:13 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CalculadoraCalvo from './CalculadoraCalvo'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CalculadoraCalvo />
+  </React.StrictMode>,
+)
